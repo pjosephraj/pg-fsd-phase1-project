3e58d52aead04ee898f21372cc8e54a393714966
pjosephraj <mailjosephraj@gmail.com>
2022-07-04 22:05:42 +0530

    Project done...

--- src/com/phase1/project.java
+++ src/com/phase1/project.java
@@ -1,16 +1,15 @@
 package com.phase1;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.FilenameFilter;
 import java.util.Scanner;
 
-// Name of Projects
-// Number of Sprints 2
-// Number of User Stories 3
-// Number of tasks: 6
-//Virendra Sharma (tedx speaker-black coat)
-
 public class project {
-    public static void main(String[] args) {
 
+    public static File usrFolder = new File("user-folder");
+
+    public static void main(String[] args) {
         System.out.println("Project: PG FSD Implement OOPS using JAVA with Data Structures and Beyond.");
         System.out.println("==========================================================================");
         mainOptions();
@@ -28,32 +27,25 @@ public class project {
         char escSwitch = 'Y';
 
         do {
-            switch(optionSelected) {
+            switch (optionSelected) {
                 case 1:
-                    System.out.println("1 is Selected");
+                    listFiles();
                     escSwitch = 'N';
+                    System.exit(0);
                     break;
                 case 2:
-                    System.out.println("2 is Selected");
-                    escSwitch = 'N';
                     secondaryOptions();
+                    escSwitch = 'N';
                     break;
                 case 3:
-                    System.out.println("3 is Selected");
                     escSwitch = 'N';
+                    System.exit(0);
                     break;
                 default:
                     System.out.println("Wrong Option, Please choose 1 to 3.");
                     optionSelected = inp.nextInt();
             }
         } while (escSwitch == 'Y');
-    }
-
-    public static boolean isToContinue() {
-        Scanner inp = new Scanner(System.in);
-        System.out.println("Do you want to Continue?");
-        char option = inp.next().charAt(0);
-        return (option == 'Y' || option == 'y');
     }
 
     public static void secondaryOptions() {
@@ -72,27 +64,25 @@ public class project {
         char escSwitch = 'Y';
 
         do {
-            switch(optionSelected) {
+            switch (optionSelected) {
                 case 1:
-                    System.out.println("1 is Selected Option");
-                    escSwitch = 'N';
+                    addFile();
+                    secondaryOptions();
                     break;
                 case 2:
-                    System.out.println("2 is Selected Option");
-                    escSwitch = 'N';
+                    deleteFile();
+                    secondaryOptions();
                     break;
                 case 3:
-                    System.out.println("3 is Selected Option");
-                    escSwitch = 'N';
+                    searchFile();
+                    secondaryOptions();
                     break;
                 case 4:
-                    System.out.println("4 is Selected Option");
-                    escSwitch = 'N';
                     System.out.println("=================================\n");
+                    escSwitch = 'Y';
                     mainOptions();
                     break;
                 case 5:
-                    System.out.println("5 is Selected Option");
                     escSwitch = 'N';
                     System.exit(0);
                     break;
@@ -100,12 +90,91 @@ public class project {
                     System.out.println("Wrong Option, Please choose 1 to 5.");
                     optionSelected = inp.nextInt();
             }
-
-            boolean toContinue = isToContinue();
-            if(toContinue) {
-                System.out.println("=================================\n");
-                mainOptions();
-            }
         } while (escSwitch == 'Y');
+    }
+
+    public static void listFiles() {
+        System.out.println("List of Files: ");
+        try {
+            String folderPath = usrFolder.getAbsolutePath();
+            File filesFolder = new File(folderPath);
+            String[] files = filesFolder.list();
+            if (files.length > 0) {
+                for (String file : files) {
+                    System.out.println(file);
+                }
+            } else {
+                System.out.println(" - No Files Found! - ");
+            }
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
+    }
+
+    public static void addFile() {
+        System.out.println("Please Enter File Name: ");
+        Scanner inp = new Scanner(System.in);
+        String fileName = inp.next();
+        try {
+            FileWriter addedFile = new FileWriter(usrFolder.getAbsolutePath() + "\\" + fileName);
+            addedFile.write(fileName + ": created by user");
+            addedFile.close();
+            System.out.println(fileName + " file created");
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
+    }
+
+    public static void deleteFile() {
+        System.out.println("Please Enter File Name to Delete: ");
+        Scanner inp = new Scanner(System.in);
+        String fileName = inp.next();
+        try {
+            String folderPath = usrFolder.getAbsolutePath();
+            File folderToCheck = new File(folderPath);
+            File[] fileFound = folderToCheck.listFiles(new CheckFileName(fileName));
+            if (fileFound.length == 1) {
+                File file = new File(folderPath + "\\" + fileName);
+                file.delete();
+            } else {
+                System.out.println("File Not Found!");
+            }
+
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
+    }
+
+    public static void searchFile() {
+        System.out.println("Please Enter File Name to Search: ");
+        Scanner inp = new Scanner(System.in);
+        String fileName = inp.next();
+
+        try {
+            String folderPath = usrFolder.getAbsolutePath();
+            File folderToCheck = new File(folderPath);
+            File[] fileFound = folderToCheck.listFiles(new CheckFileName(fileName));
+            if (fileFound.length == 1) {
+                System.out.println("File is Exist.");
+            } else {
+                System.out.println("File Not Found!");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getStackTrace());
+        }
+    }
+}
+
+class CheckFileName implements FilenameFilter {
+    private String fileName;
+
+    public CheckFileName(String name) {
+        this.fileName = name.toLowerCase();
+    }
+
+    @Override
+    public boolean accept(File dir, String name) {
+        String nameLowercase = name.toLowerCase();
+        return nameLowercase.startsWith(this.fileName) && nameLowercase.endsWith(this.fileName);
     }
 }
